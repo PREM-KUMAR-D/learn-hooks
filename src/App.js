a39c1413132584ba2d4d0eaea532453027860002
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-11-14 12:21:30 +0530

    using context to reduce prop drilling so that we can make our components faster

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { useState ,useEffect} from "react";
+import React, { useState, useEffect } from "react";
+import AuthContext from "./store/auth-context";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
-  const [isLoggedIn,setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(()=>{
+  useEffect(() => {
     const storedLoggedIn = localStorage.getItem("isLoggedIn");
-    if(storedLoggedIn === "1"){
+    if (storedLoggedIn === "1") {
       setIsLoggedIn(true);
     }
 
-  },[])
+  }, [])
 
-  
-  const logInHandler =(email,password)=>{
-    
+
+  const logInHandler = (email, password) => {
+
 
     setIsLoggedIn(true);
-    localStorage.setItem("isLoggedIn","1");
+    localStorage.setItem("isLoggedIn", "1");
   }
 
-  const logOutHandler = ()=>{
+  const logOutHandler = () => {
     setIsLoggedIn(false);
     localStorage.removeItem("isLoggedIn");
   }
@@ -30,17 +31,17 @@ function App() {
 
 
   return (
-    <>
-      <MainHeader 
-        isAuthenticated={isLoggedIn}
-        onLogout={logOutHandler}
-      />
+    <AuthContext.Provider value={{
+      isLoggedIn: isLoggedIn,
+      onLogout: logOutHandler,
+    }}
+    >
+      <MainHeader />
       <main>
         {!isLoggedIn && <Login onLogin={logInHandler} />}
         {isLoggedIn && <Home onLogout={logOutHandler} />}
       </main>
-
-    </>
+    </AuthContext.Provider>
   );
 }
 
